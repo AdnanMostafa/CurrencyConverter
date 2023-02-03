6afcaeeb636853a567e776ea424b1369a0b902d0
Adnan <adnanmostafaprofessional@gmail.com>
2023-02-02 20:48:12 -0500

    Add project files.

---
+++ MyProject01/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace MyProject01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtBoxAmnt = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.lblRate = new System.Windows.Forms.Label();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.cmbIndex = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat ExtraBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(92, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(380, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to our first project";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Location = new System.Drawing.Point(98, 129);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(46, 13);
+            this.lblAmount.TabIndex = 1;
+            this.lblAmount.Text = "Amount:";
+            this.lblAmount.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnConvert.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnConvert.Location = new System.Drawing.Point(150, 228);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(240, 23);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtBoxAmnt
+            // 
+            this.txtBoxAmnt.Location = new System.Drawing.Point(150, 126);
+            this.txtBoxAmnt.Name = "txtBoxAmnt";
+            this.txtBoxAmnt.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxAmnt.TabIndex = 3;
+            this.txtBoxAmnt.TextChanged += new System.EventHandler(this.txtBoxAmnt_TextChanged);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(98, 194);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(40, 13);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "Result:";
+            this.lblResult.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(150, 191);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 5;
+            // 
+            // lblRate
+            // 
+            this.lblRate.AutoSize = true;
+            this.lblRate.Location = new System.Drawing.Point(98, 161);
+            this.lblRate.Name = "lblRate";
+            this.lblRate.Size = new System.Drawing.Size(33, 13);
+            this.lblRate.TabIndex = 6;
+            this.lblRate.Text = "Rate:";
+            this.lblRate.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // txtRate
+            // 
+            this.txtRate.Location = new System.Drawing.Point(150, 158);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.Size = new System.Drawing.Size(100, 20);
+            this.txtRate.TabIndex = 7;
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(98, 270);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(53, 13);
+            this.lblMsg.TabIndex = 8;
+            this.lblMsg.Text = "Message:";
+            this.lblMsg.Click += new System.EventHandler(this.label2_Click_2);
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(154, 270);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(0, 13);
+            this.lblInput.TabIndex = 9;
+            // 
+            // cmbIndex
+            // 
+            this.cmbIndex.FormattingEnabled = true;
+            this.cmbIndex.Items.AddRange(new object[] {
+            "BDT",
+            "CAD",
+            "GBP",
+            "INR",
+            "EUR",
+            "AUD"});
+            this.cmbIndex.Location = new System.Drawing.Point(269, 158);
+            this.cmbIndex.Name = "cmbIndex";
+            this.cmbIndex.Size = new System.Drawing.Size(121, 21);
+            this.cmbIndex.TabIndex = 10;
+            this.cmbIndex.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbIndex);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.txtRate);
+            this.Controls.Add(this.lblRate);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtBoxAmnt);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "My First Project";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtBoxAmnt;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label lblRate;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.ComboBox cmbIndex;
+    }
+}
+
---
+++ MyProject01/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyProject01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string text=txtBoxAmnt.Text;
+            double amount = Convert.ToDouble(text);
+
+            string rateText = txtRate.Text;
+            double rate= Convert.ToDouble(rateText);
+
+
+            if (amount < 0 && rate < 0)
+            {
+                lblInput.Text = "Both Amount and Rate Input is Invalid";
+                return;
+            }
+            else if (amount < 0)
+            {
+                lblInput.Text = "Your Amount Input is Invalid";
+                return;
+            }
+            else if (rate < 0)
+            {
+                lblInput.Text = "Your Rate Input is Invalid";
+                return;
+            }
+
+            //result.ToString("0.00")
+
+            double result;
+            result = amount * rate;
+
+            //txtResult.Text = Convert.ToString(result);       
+            //lblInput.Text= "Your Converted Amount is: " + Convert.ToString(result) + "/-";
+
+            txtResult.Text = result.ToString("F2");
+            lblInput.Text = "Your Converted Amount from USD is: " + result.ToString("F2") + "/-";
+
+            
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBoxAmnt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbIndex.SelectedIndex == 0) //BDT
+            {
+                txtRate.Text = "104.92";
+            }
+            else if (cmbIndex.SelectedIndex == 1) //CAD
+            {
+                txtRate.Text = "1.33";
+            }
+            else if (cmbIndex.SelectedIndex == 2) //GBP
+            {
+                txtRate.Text = "0.81";
+            }
+            else if (cmbIndex.SelectedIndex == 3) //INR
+            {
+                txtRate.Text = "81.81";
+            }
+            else if (cmbIndex.SelectedIndex == 4) //EUR
+            {
+                txtRate.Text = "0.91";
+            }
+            else if (cmbIndex.SelectedIndex == 5) //AUD
+            {
+                txtRate.Text = "1.40";
+            }
+        }
+    }
+}
